9d0a9575a3dfc8b3a0320ffd99c06e391975eeaa
ChinonyeECOS101 <chinonye.emmanuel@pau.edu.ng>
2025-12-03 16:34:15 +0100

    passing value from a variable to a function

---
+++ week-10/practice_2/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let v = vec![10, 20, 30];
+    //vector v owns the object in heap
+
+    let v2 = v;         // moves ownership to v2
+
+    dispaly(v2);
+    // v2 is moved to display and v2 is invalidated 
+
+
+    // v2 is no longer usable here
+}
+fn dispaly(dis:Vec<i32>){
+    println!("{:?}",dis );
+}
